f5f1e0ca401defa0b3c8d2a176d4dfe5ade0ced3
damimonge98 <69829575+damimonge98@users.noreply.github.com>
2021-02-11 14:12:58 -0300

    unlisted mision

--- my-web/src/components/about.jsx
+++ my-web/src/components/about.jsx
@@ -38,13 +38,12 @@ export default function About () {
 
 				<div className = "DivsProp">
 					<h2 className = "title"> PRINCIPALES OBJETIVOS </h2>
-					<ul className = "textAbout">
-					<li>Brindar un alto nivel de servicios adecuados para personas que buscan experiencias relajantes, cómodas y memorables en la industria hotelera y turística.</li>
-					<li>Evaluar los problemas culturales, económicos y sociales actuales que afectan la industria del turismo y los viajes.</li>
-					<li>Ofrecer los servicios de viaje a precios muy razonables.</li>
-					<li>Ser una empresa conocida por su honestidad y transparencia al brindarle las mejores soluciones de viaje para su negocio.</li>
+					Brindar un alto nivel de servicios adecuados para personas que buscan experiencias relajantes, cómodas y memorables en la industria hotelera y turística.
+					Evaluar los problemas culturales, económicos y sociales actuales que afectan la industria del turismo y los viajes.
+					Ofrecer los servicios de viaje a precios muy razonables.
+					Ser una empresa conocida por su honestidad y transparencia al brindarle las mejores soluciones de viaje para su negocio.
 					<img src = "https://previews.123rf.com/images/cppzoe/cppzoe1710/cppzoe171000018/87607260-amigos-felices-viajes-concepto-de-expedici%C3%B3n.jpg" className = "ImgAbout" />
-					</ul>
+					
 
 
 					
